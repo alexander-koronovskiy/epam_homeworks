d5b8e8f70d9186321f49a6df993fec17bba82980
alexander <alexander.koronovskiy@gmail.com>
2020-11-04 17:36:24 +0400

    just cosmetic final

--- homework1/test_max.py
+++ homework1/test_max.py
@@ -12,8 +12,8 @@ from tasks.max_and_min import find_maximum_and_minimum
 )
 def test_max_sub_sum(file: str, expected_result: Tuple[int, int]):
     """
-    test for fib.py check_fibonacci method
-    use 'pytest test_fib.py' in console log for testing
+    test for max_and_min.py find_maximum_and_minimum method
+    use 'pytest test_max.py' in console log for testing
     """
     actual_result = find_maximum_and_minimum(file)
     assert actual_result == expected_result
