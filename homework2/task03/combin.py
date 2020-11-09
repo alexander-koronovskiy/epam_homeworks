b483cd3e623a98bd19303f56502010263e2fedaa
alexander <alexander.koronovskiy@gmail.com>
2020-11-09 20:13:44 +0400

    hw2 t3

--- homework2/task03/combin.py
+++ homework2/task03/combin.py
@@ -11,8 +11,16 @@ assert combinations([1, 2], [3, 4]) == [
     [2, 4],
 ]
 """
+import itertools
 from typing import Any, List
 
 
 def combinations(*args: List[Any]) -> List[List]:
-    return [[1, 3], [1, 4], [2, 3], [2, 4]]
+    comb1 = itertools.combinations(args[0], 1)
+    comb2 = itertools.combinations(args[1], 1)
+    combs = itertools.product(comb1, comb2)
+    result = [list(itertools.chain.from_iterable(x)) for x in combs]
+    return result
+
+
+print(combinations([1, 2], [3, 4]))
