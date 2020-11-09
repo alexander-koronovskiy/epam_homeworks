36d21e4254844a6d4521a48d8fabbb82e75710a7
alexander <alexander.koronovskiy@gmail.com>
2020-11-09 17:14:14 +0400

    task 2 hw2 init

--- homework2/task01/tests/test_handler.py
+++ homework2/task01/tests/test_handler.py
@@ -1,5 +1,5 @@
 import pytest
-from task01.handler_file import get_rarest_char
+from task01.handler_file import count_punctuation_chars, get_rarest_char
 
 
 @pytest.mark.parametrize(
@@ -13,4 +13,18 @@ def test_rarest_char(file: str, expected_result: str):
     test for handler_file.py get_rarest_char method
     """
     actual_result = get_rarest_char(file)
+    assert actual_result == expected_result
+
+
+@pytest.mark.parametrize(
+    ["file", "expected_result"],
+    [
+        ("data00.txt", 7),
+    ],
+)
+def test_punctuation_chars(file: str, expected_result: int):
+    """
+    test for handler_file.py get_rarest_char method
+    """
+    actual_result = count_punctuation_chars(file)
     assert actual_result == expected_result
