4bf3102c5deb29720ee489386a2c9535f721965e
alexander <alexander.koronovskiy@gmail.com>
2020-11-09 18:46:51 +0400

    hw2 test3

---
+++ homework2/task03/tests/test_combin.py
@@ -0,0 +1,16 @@
+from typing import Any, List
+
+import pytest
+from task03.combin import combinations
+
+
+@pytest.mark.parametrize(
+    ["inp", "expected_result"],
+    [(([1, 2], [3, 4]), [[1, 3], [1, 4], [2, 3], [2, 4]])],
+)
+def test_major_and_minor(inp: List[Any], expected_result: List[List]):
+    """
+    test for major_and_minor.py get_rarest_char method
+    """
+    actual_result = combinations(inp)
+    assert actual_result == expected_result
