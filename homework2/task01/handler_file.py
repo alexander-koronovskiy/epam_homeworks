dd681410caa90a8dc1e825d2c1e40dd48f2cc7fa
alexander <alexander.koronovskiy@gmail.com>
2020-11-19 18:48:57 +0400

    task02 rem it6

--- homework2/task01/handler_file.py
+++ homework2/task01/handler_file.py
@@ -15,7 +15,7 @@ def word_stream(file: str, encoding: str) -> Generator:
         while True:
 
             # buffer creation
-            buf = f.read(65536)
+            buf = f.read(65536).lower()
             if not buf:
                 break
 
@@ -44,16 +44,47 @@ def word_stream(file: str, encoding: str) -> Generator:
 
 
 def get_longest_diverse_words(file_path: str, encoding: str) -> List[str]:
+
+    # init searching longest diverse words
+    most_unique_words = []
+    permit_unique_len = 0
+
     for word in word_stream(file_path, encoding):
-        pass
+
+        # don't add elements, that contains very few unique symbols
+        if len(set(word)) > permit_unique_len:
+            most_unique_words.append((word, len(set(word))))
+
+        # if words less then 10, permit all recordings
+        if len(most_unique_words) > 10:
+            most_unique_words.sort(key=lambda unique: unique[1], reverse=True)
+            permit_unique_len = most_unique_words.pop(-1)[1]
+
+    return [i[0] for i in most_unique_words]
 
 
-def get_rarest_char(file_path: str, encoding: str) -> str:
-    pass
+def get_rarest_char(file_path, encoding) -> str:
+
+    # it saves symbol and occurrence
+    ch_map = {}
+
+    for word in word_stream(file_path, encoding):
+        for ch in word:
+            if ch in ch_map:
+                ch_map[ch] += 1
+            else:
+                ch_map[ch] = 1
+
+    min_unique = sorted(ch_map.items(), key=lambda item: item[1])
+    return min_unique[0][0]
 
 
 def count_punctuation_chars(file_path: str, encoding: str) -> int:
-    pass
+    count = 0
+    for word in word_stream(file_path, encoding):
+        if len(word) == 1:
+            count += 1
+    return count
 
 
 def count_non_ascii_chars(file_path: str, encoding: str) -> int:
@@ -65,5 +96,7 @@ def get_most_common_non_ascii_char(file_path: str, encoding: str) -> str:
 
 
 isascii = lambda s: len(s) == len(s.encode())
-for word in word_stream(file="data.txt", encoding="unicode-escape"):
-    print(word)
+# for word in word_stream(file="data.txt", encoding="unicode-escape"): print(word)  # 0
+# print(get_longest_diverse_words("simple_data.txt", encoding="utf-8"))  # 1
+# print(get_rarest_char("simple_data.txt", encoding="utf-8"))  # 2
+print(count_punctuation_chars("simple_data.txt", encoding="utf-8"))
