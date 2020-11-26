178e20f0749324e7d08a0bfdb19f823fc5f8549f
alexander <alexander.koronovskiy@gmail.com>
2020-11-26 15:17:05 +0400

    hw2 rem t5 it1

--- homework2/task05/alph.py
+++ homework2/task05/alph.py
@@ -8,29 +8,12 @@ assert = custom_range(string.ascii_lowercase, 'g') == ['a', 'b', 'c', 'd', 'e', 
 assert = custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
 assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', 'j', 'h']
 """
-import string
 from typing import Any, List
 
 
 def custom_range(
-    alphabet: str, start: str = "a", end: str = "z", pos: int = -2
-) -> List[str]:
-
-    count, pos_start, pos_end = 0, 1, 26
-
-    for i in alphabet:
-        if i == start:
-            pos_start = count
-        if i == end:
-            pos_end = count
-        count += 1
-
-    return [i for i in alphabet[pos_start:pos_end:pos]]
-
-
-def custom_range_2(
     seq: Any, key_word_first: Any = None, key_word_last: Any = None, step: Any = None
-):
+) -> List[Any]:
 
     first_el, last_el = None, None
 
@@ -44,9 +27,3 @@ def custom_range_2(
         return [i for i in seq[0:first_el:step]]
 
     return [i for i in seq[first_el:last_el:step]]
-
-
-print(custom_range_2(string.ascii_lowercase))
-print(custom_range_2(string.ascii_lowercase, "g"))
-print(custom_range_2(string.ascii_lowercase, "g", "p"))
-print(custom_range(string.ascii_lowercase, "p", "g", -2))
--- homework2/task05/tests/test_alph.py
+++ homework2/task05/tests/test_alph.py
@@ -8,6 +8,7 @@ from task05.alph import custom_range
     ["alphabet", "start", "end", "pos", "expected_result"],
     [
         (string.ascii_lowercase, "p", "g", -2, ["p", "n", "l", "j", "h"]),
+        ([1, 2, 3, 4, 5], 5, 2, -1, [5, 4, 3]),
     ],
 )
 def test_custom_range(alphabet, start, end, pos, expected_result):
