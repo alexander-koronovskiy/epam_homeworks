939cbb9c3da860eada0af2492bb79050d96d74a3
alexander <alexander.koronovskiy@gmail.com>
2020-11-28 23:07:39 +0400

    hw3 t1 rem it3

--- homework3/task01/tests/test_cache.py
+++ homework3/task01/tests/test_cache.py
@@ -6,9 +6,9 @@ def foo(a, b):
     return a * b
 
 
-def test_memoize():
-    foo(2, 4)
-    foo("hi", 3)
+def test_memoize():  # times =2
+    foo(2, 4)  # first call
+    foo("hi", 3)  # second call
     assert foo == {(2, 4): 8, ("hi", 3): "hihihi"}
-    foo("g", 4)
+    foo("g", 4)  # third call
     assert foo == {("g", 4): "gggg"}
