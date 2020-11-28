b3baa5224aba4d282c0eade32472b35e7a17ca22
alexander <alexander.koronovskiy@gmail.com>
2020-11-28 22:19:38 +0400

    hw3 t1 rem it0

--- homework3/task01/wrap.py
+++ homework3/task01/wrap.py
@@ -1,38 +1,25 @@
-"""
-In previous homework task 4, you wrote a cache function
-that remembers other function output value.
-Modify it to be a parametrized decorator,
-so that the following code:
-"""
-from collections import deque
+class memoize(dict):
+    """
+    That's a memoizing function that works on functions, methods, or classes,
+    and exposes the cache publicly.
+    """
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        return self[args]
+
+    def __missing__(self, key):
+        result = self[key] = self.func(*key)
+        return result
 
 
-class Memoized:
-    def __init__(self, cache_size=100):
-        self.cache_size = cache_size
-        self.call_args_queue = deque()
-        self.call_args_to_result = {}
+@memoize
+def foo(a, b):
+    return a * b
 
-    def __call__(self, fn):
-        def new_func(*args, **kwargs):
-            memoization_key = self._convert_call_arguments_to_hash(args, kwargs)
-            if memoization_key not in self.call_args_to_result:
-                result = fn(*args, **kwargs)
-                self._update_cache_key_with_value(memoization_key, result)
-                self._evict_cache_if_necessary()
-            return self.call_args_to_result[memoization_key]
 
-        return new_func
-
-    def _update_cache_key_with_value(self, key, value):
-        self.call_args_to_result[key] = value
-        self.call_args_queue.append(key)
-
-    def _evict_cache_if_necessary(self):
-        if len(self.call_args_queue) > self.cache_size:
-            oldest_key = self.call_args_queue.popleft()
-            del self.call_args_to_result[oldest_key]
-
-    @staticmethod
-    def _convert_call_arguments_to_hash(args, kwargs):
-        return hash(str(args) + str(kwargs))
+foo(2, 4)
+foo("hi", 3)
+print(foo)
