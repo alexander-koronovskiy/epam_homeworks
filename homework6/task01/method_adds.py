9cb7f04230e9fcacf6eb8eaa2fc09ca36b8a4c16
alexander <alexander.koronovskiy@gmail.com>
2020-11-29 00:51:26 +0400

    hw6 t1 it4

--- homework6/task01/method_adds.py
+++ homework6/task01/method_adds.py
@@ -8,50 +8,26 @@ reset_instances_counter - сбрасывает счетчик экземпляр
 """
 
 
-class countcalls:
-    "Decorator that keeps track of the number of times a function is called."
+class InsertMethod(object):
+    def __init__(self, methodToInsert):
+        self.methodToInsert = methodToInsert
 
-    __instances = {}
+    def __call__(self, classObject):
+        def wrapper(*args, **kwargs):
+            setattr(classObject, self.methodToInsert.__name__, self.methodToInsert)
+            return classObject(*args, **kwargs)
 
-    def __init__(self, f):
-        self.__f = f
-        self.__numcalls = 0
-        countcalls.__instances[f] = self
-
-    def __call__(self, *args, **kwargs):
-        self.__numcalls += 1
-        return self.__f(*args, **kwargs)
-
-    def count(self):
-        "Return the number of times the function f was called."
-        return countcalls.__instances[self.__f].__numcalls
-
-    @staticmethod
-    def counts():
-        "Return a dict of {function: # of calls} for all registered functions."
-        return dict(
-            [
-                (f.__name__, countcalls.__instances[f].__numcalls)
-                for f in countcalls.__instances
-            ]
-        )
+        return wrapper
 
 
-# example
-@countcalls
-def f():
-    return "f called"
+def IWillBeInserted(self):
+    print("Success")
 
 
-@countcalls
-def g():
-    return "g called"
+@InsertMethod(IWillBeInserted)
+class Something(object):
+    def __init__(self):
+        pass
 
-
-f()
-f()
-f()
-print(f.count())  # prints 3
-print(countcalls.counts())  # same as f.counts() or g.counts()
-g()
-print(g.count())  # prints 1
+    def action(self):
+        self.IWillBeInserted()
--- homework6/task01/tests/test_example.py
+++ homework6/task01/tests/test_example.py
@@ -1,18 +1,14 @@
-from task01.method_adds import countcalls
+"""
+Написать декоратор instances_counter, который применяется к любому классу
+и добавляет ему 2 метода:
+get_created_instances - возвращает количество созданых экземпляров класса
+reset_instances_counter - сбросить счетчик экземпляров,
+возвращает значение до сброса
+Имя декоратора и методов не менять
+Ниже пример использования
+"""
 
+from task01.method_adds import Something
 
-def instances_counter(cls):
-    """Some code"""
-    return cls
-
-
-@countcalls
-class User:
-    pass
-
-
-if __name__ == "__main__":
-    User.get_created_instances()  # 0
-    user, _, _ = User(), User(), User()
-    user.get_created_instances()  # 3
-    user.reset_instances_counter()  # 3
+a = Something()
+a.action()
