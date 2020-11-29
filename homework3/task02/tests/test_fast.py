28113dae6cf1a8ae272596982ed36ba4927af102
alexander <alexander.koronovskiy@gmail.com>
2020-11-30 00:52:51 +0400

    hw3 t2 it2

--- homework3/task02/tests/test_fast.py
+++ homework3/task02/tests/test_fast.py
@@ -1,9 +1,3 @@
-"""
-Декоратор instances_counter применяется к любому классу и добавляет ему 2 метода:
-get_created_instances - возвращает количество созданых экземпляров класса
-reset_instances_counter - сбрасывает счетчик экземпляров и возвращает значение до сброса
-Имя декоратора и методов неизменно
-"""
 from task02.fast_calc import benchmark, slow_calculate
 
 
@@ -12,4 +6,4 @@ def total_sum(n):
     return sum([slow_calculate(i) for i in range(n)])
 
 
-print(total_sum(5))  # распаралеллить вычисления
+print(total_sum(3))
