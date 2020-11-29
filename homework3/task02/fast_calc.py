6ea11dbcce81ce9157a94974d5c7dc918664a933
alexander <alexander.koronovskiy@gmail.com>
2020-11-29 23:37:38 +0400

    hw3 t2 rem it0

--- homework3/task02/fast_calc.py
+++ homework3/task02/fast_calc.py
@@ -1,24 +1,16 @@
+"""
+Calculate total sum of slow_calculate() of all numbers starting from 0 to 500.
+Calculation time should not take more than a minute.
+Use functional capabilities of multiprocessing module.
+
+посчитать сумму
+"""
+
+
 import hashlib
 import random
 import struct
 import time
-from threading import Thread
-
-
-class MyThread(Thread):
-    """
-    A threading example
-    """
-
-    def __init__(self, name):
-        """Thread initialization"""
-        Thread.__init__(self)
-        self.name = name
-
-    def run(self):
-        """thread start"""
-        msg = self.name
-        slow_calculate(msg)
 
 
 def slow_calculate(value):
@@ -26,12 +18,3 @@ def slow_calculate(value):
     time.sleep(random.randint(1, 3))
     data = hashlib.md5(str(value).encode()).digest()
     return sum(struct.unpack("<" + "B" * len(data), data))
-
-
-def create_threads(n: int):
-    """
-    threads group creation
-    """
-    for i in range(n):
-        my_thread = MyThread(i)
-        my_thread.start()
--- homework3/task02/tests/test_fast.py
+++ homework3/task02/tests/test_fast.py
@@ -1,13 +1,34 @@
-import timeit
+"""
+Декоратор instances_counter применяется к любому классу и добавляет ему 2 метода:
+get_created_instances - возвращает количество созданых экземпляров класса
+reset_instances_counter - сбрасывает счетчик экземпляров и возвращает значение до сброса
+Имя декоратора и методов неизменно
+"""
+import requests
 
-from task02.fast_calc import *
+
+def benchmark(func):
+    import time
+
+    def wrapper(*args, **kwargs):
+
+        start = time.time()
+
+        return_value = func(*args, **kwargs)
+
+        end = time.time()
+        print("[*] Время выполнения: {} секунд.".format(end - start))
+
+        return return_value
+
+    return wrapper
 
 
-def test_calculate():
-    """
-    test for fast_calc.py
-    """
-    start_calc = timeit.default_timer()
-    create_threads(500)
-    end_calc = timeit.default_timer()
-    assert end_calc - start_calc < 60
+@benchmark
+def fetch_webpage(url):
+    webpage = requests.get(url)
+    return webpage.text
+
+
+webpage = fetch_webpage("https://google.com")
+# print(webpage)
