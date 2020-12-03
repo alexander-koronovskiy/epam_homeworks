563709f758b4bf6155df8da8a64dd0511db106e1
alexander <alexander.koronovskiy@gmail.com>
2020-12-03 22:43:22 +0400

    hw4 t2 it3 rem

--- homework4/task02/base_parser.py
+++ homework4/task02/base_parser.py
@@ -15,26 +15,14 @@ import time
 import requests
 
 
-def benchmark(func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        return_value = func(*args, **kwargs)
-        end = time.time()
-        print("[*] Время выполнения: {} секунд.".format(end - start))
-        return return_value
-
-    return wrapper
-
-
-@benchmark
-def fetch_webpage(url):
-    webpage = requests.get(url)
-    return webpage.text
+def process_response(url):
+    r = requests.get(url)
+    return r.content
 
 
 def count_dots_on_i(url: str) -> int:
     count = 0
-    webpage = fetch_webpage(url)
+    webpage = process_response(url)
     for i in str(webpage):
         if i == "i":
             count += 1
--- homework4/task02/tests/test_parser.py
+++ homework4/task02/tests/test_parser.py
@@ -1,19 +1,35 @@
-from unittest import mock
+from unittest import TestCase, mock
 
-from task02.base_parser import fetch_webpage
+import requests
+from mock import patch
+from task02.base_parser import count_dots_on_i, process_response
 
 
-def test_fetch_with_mock():
-    with mock.patch("requests.get") as mock_get:
+def url_exists(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return True
 
-        username = "https:/www.example.com"
-        mock_get.return_value.ok = True
+    elif r.status_code == 404:
+        return False
 
-        json_response = (
-            "<html><head>A song</head><body>House of the Rising Sun</body></html>"
-        )
-        mock_get.return_value.json.return_value = json_response
 
-        info = fetch_webpage(username)
-        assert info is not None
-        assert json_response == info
+print(count_dots_on_i("https://example.com"))
+
+
+class FetchTests(TestCase):
+    def test_returns_true_if_url_found(self):
+        with patch("requests.get") as mock_request:
+            url = "http://google.com"
+            mock_request.return_value.status_code = 200
+
+            self.assertTrue(url_exists(url))
+            mock_request.assert_called_once_with(url)
+
+    def test_returns_false_if_url_not_found(self):
+        with patch("requests.get") as mock_request:
+            url = "http://google.com/nonexistingurl"
+            mock_request.return_value.status_code = 404
+
+            self.assertFalse(url_exists(url))
+            mock_request.assert_called_once_with(url)
