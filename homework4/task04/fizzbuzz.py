363745867cb3e0fe02e1b216b0dd3719fb4ecf04
alexander <alexander.koronovskiy@gmail.com>
2020-12-04 01:26:04 +0400

    hw4 t4 rem

--- homework4/task04/fizzbuzz.py
+++ homework4/task04/fizzbuzz.py
@@ -1,23 +1,58 @@
 """
-Это модуль-пример.
+This is the "example" module.
+The example module supplies one function, factorial().  For example,
+>>> fizzbuzz(5)
+[1, 2, 'Fizz', 4, 'Buzz']
 
-Этот модуль предоставляет одну функцию - factorial().  Например,
+That is complete insturction to how testing launch:
 
->> factorial(5)
-120
+- visit website https://www.python.org/
+- click 'Download' for install latest python version
+- find python.exe on 'downloads' in your computer
+- click python.exe and install it with default setting
+- use github functional for copy that code or
+copy this example in .txt file and then rename doc as 'fizzbuzz.py'
+- good! At this step you have file 'fizzbuzz.py' on your computer
+- open your computer command line (Ctrl+Alt+T in Linux, win+r cmd in Windows)
+- print command 'python fizzbuzz.py -v'
+- Best wishes!
 """
 
 
-from typing import List
+def fizzbuzz(n):
+    """Return the fizzbuzz sequiency of n, an exact integer >= 0.
 
+    >>> fizzbuzz(6)
+    [1, 2, 'Fizz', 4, 'Buzz', 'Fizz']
+    >>> fizzbuzz(15)
+    [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
+    >>> fizzbuzz(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: n must be > 0
 
-def fb(n: int) -> List:
+    Factorials of floats are OK, but the float must be an exact integer:
+    >>> fizzbuzz(10.1)
+    Traceback (most recent call last):
+        ...
+    ValueError: n must be exact integer
+
+    It must also not be ridiculously large:
+    >>> fizzbuzz(1e100)
+    Traceback (most recent call last):
+        ...
+    OverflowError: n too large
     """
-    The standard program realization for calculation FizzBuzz sequence
 
-    :param n: len of FizzBuzz sequence set by user
-    :return: FizzBuzz sequence
-    """
+    import math
+
+    if not n > 0:
+        raise ValueError("n must be > 0")
+    if math.floor(n) != n:
+        raise ValueError("n must be exact integer")
+    if n + 1 == n:  # catch a value like 1e300
+        raise OverflowError("n too large")
+
     result = []
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
@@ -31,3 +66,9 @@ def fb(n: int) -> List:
             continue
         result.append(i)
     return result
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
