ed294b96f6852ad8312cdd509dc6f3ec02490b3d
alexander <alexander.koronovskiy@gmail.com>
2020-12-03 14:22:32 +0400

    hw4 t2 rem it1

--- homework4/task02/tests/test_parser.py
+++ homework4/task02/tests/test_parser.py
@@ -1,7 +1,15 @@
-from task02.base_parser import *
+from task02.base_parser import count_dots_on_i
 
 
-def test_parser():
+def connection_mock(url):
+    return {"message": "OK"}
+
+
+def test_connection():
+    assert connection_mock("https://example.com/") == {"message": "OK"}
+
+
+def test_count():
     """
     comparison of function results with measured value
     """
