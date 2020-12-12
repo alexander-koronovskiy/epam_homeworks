15133785fe5cc7635d3a2645791b878cf013c6fa
alexander <alexander.koronovskiy@gmail.com>
2020-12-12 20:46:27 +0400

    hw8 t2 it5

--- homework8/task02/db_handler.py
+++ homework8/task02/db_handler.py
@@ -10,10 +10,6 @@ object implements iteration protocol. i.e. you could use it in for loops::
 import sqlite3 as lite
 from typing import Callable, Generator, List
 
-# реализовать доступ к элементу +++
-# реализовать протокол __len__
-# спросить про mock-тестирование
-
 
 def dict_factory(cursor, row):
     d = {}
@@ -27,54 +23,37 @@ class TableHandle:
         self.base = base
         self.table = table
 
-    def _with_cursor(self, fn: Callable):
+    def _get_connection(self):
         con = lite.connect(self.base)
         con.row_factory = lite.Row  # or use dict_factory
-        with con:
-            cur = con.cursor()
-            return fn(cur)
+        return con
 
     def __len__(self):
-        return self._with_cursor(
-            lambda cur: cur.execute(
-                f"SELECT COUNT(*) AS c FROM {self.table}"
-            ).fetchone()
-        )["c"]
+        with self._get_connection() as con:
+            cur = con.cursor()
+            return cur.execute(f"SELECT COUNT(*) AS c FROM {self.table}").fetchone()[
+                "c"
+            ]
 
     def __getitem__(self, row):
-        return self._with_cursor(
-            lambda cur: cur.execute(
+        with self._get_connection() as con:
+            cur = con.cursor()
+            return cur.execute(
                 f"SELECT * FROM {self.table} WHERE name = '{row}'"
             ).fetchone()
-        )
 
     def __iter__(self):
-        return iter(
-            self._with_cursor(lambda cur: cur.execute(f"SELECT * FROM {self.table}"))
-        )
+        with self._get_connection() as con:
+            cur = con.cursor()
+            for item in cur.execute(f"SELECT * FROM {self.table}"):
+                yield item
 
-    def __getattr__(self, row):
-        return elem_reader(self.base, row, self.table)
-
-
-def row_reader(database: str, row: str, table: str) -> Generator:
-    con = lite.connect(database)
-    with con:
-        cur = con.cursor()
-        cur.execute("SELECT {} FROM {}".format(row, table))
-        rows = cur.fetchall()
-        for row in rows:
-            yield row[0]
-
-
-# # new 'name' query access realisation
-def elem_reader(database: str, row: str, table: str) -> List:
-    con = lite.connect(database)
-    with con:
-        cur = con.cursor()
-        cur.execute("SELECT * FROM {} WHERE name = '{}'".format(table, row))
-        rows = cur.fetchall()
-        return rows
+    def __contains__(self, item):
+        with self._get_connection() as con:
+            cur = con.cursor()
+            return cur.execute(
+                f"SELECT * FROM {self.table} WHERE name = '{item}' LIMIT 1"
+            ).fetchall()
 
 
 presidents = TableHandle("example.sqlite", "presidents")
@@ -82,3 +61,4 @@ print("len: ", len(presidents))
 print("name: ", dict(presidents["Yeltsin"]))
 for president in presidents:
     print(president["name"])
+print("Putin" in presidents)
