a17a661e60b8c1b172710ecf8eeb118e99b6d081
alexander <alexander.koronovskiy@gmail.com>
2020-12-14 00:12:26 +0400

    hw9 t1 it0

--- homework9/task01/merge_sort_files.py
+++ homework9/task01/merge_sort_files.py
@@ -6,11 +6,10 @@ file2.txt: 2 4 6
 [1, 2, 3, 4, 5, 6]
 """
 from pathlib import Path
+from typing import Iterator, List, Union
 
-# from typing import List, Union, Iterator
 
-
-def readInChunks(fileObj, chunkSize=2048):
+def readInChunks(fileObj: str, chunkSize: int = 2048) -> Iterator:
     """
     Lazy function to read a file piece by piece.
     Default chunk size: 2kB.
@@ -23,8 +22,28 @@ def readInChunks(fileObj, chunkSize=2048):
         yield data
 
 
+def int_handler(a: str) -> List:
+    """
+    search int values at string
+    it works with no divided value
+    :return:
+    """
+    num_list = []
+
+    num = ""
+    for char in a:
+        if char.isdigit() or char == "-":
+            num = num + char
+        else:
+            if num != "":
+                num_list.append(int(num))
+                num = ""
+    if num != "":
+        num_list.append(int(num))
+    return num_list
+
+
 f1 = open("file1.txt")
-f2 = open("file2.txt")
-for chunk in zip(readInChunks(f1), readInChunks(f2)):
-    print(chunk)
+for chunk in readInChunks(f1):
+    print(sorted(int_handler(chunk)))  # преобразовать чиса в строке
 f1.close()
