00224e5056c80fcd5548fcf611005dab51566dd2
alexander <alexander.koronovskiy@gmail.com>
2020-12-14 17:57:24 +0400

    hw9 t3 it1

--- homework9/task03/count_file_lines.py
+++ homework9/task03/count_file_lines.py
@@ -3,9 +3,6 @@ Write a function that takes directory path, a file extension and an optional tok
 It will count lines in all files with that extension if there are no tokenizer.
 If a the tokenizer is not none, it will count tokens.
 """
-
-# задача 2 посчитать количество строк в них
-
 import os
 from pathlib import Path
 from typing import Callable, Optional
@@ -14,13 +11,16 @@ from typing import Callable, Optional
 def universal_file_counter(
     dir_path: Path, file_extension: str, tokenizer: Optional[Callable] = None
 ) -> int:
-    pass
+    os.chdir(dir_path)
+    # print(os.path.dirname(__file__))
+    count = 0
+    for item in Path(".").glob("*"):
+        if item.is_file() and str(item)[-len(file_extension) :] == file_extension:
+            # counting lines
+            with open(item) as f:
+                size = sum(1 for _ in f)
+            count += size
+    return count
 
 
-os.chdir("D:\dev\epam courses\homework9")
-
-count = 0
-for item in Path(".").glob("*"):
-    if item.is_file() and str(item)[-3:] == "txt":
-        count += 1
-print(count)
+print(universal_file_counter("D:\dev\epam courses\homework9\\task03", "txt"))
---
+++ homework9/task03/tests/test_count_lines.py
@@ -0,0 +1,3 @@
+from task03.count_file_lines import os, universal_file_counter
+
+print(os.path.dirname(__file__))
