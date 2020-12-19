e4864414e5197ec4c7f9fa7e161bf6fe5526e391
alexander <alexander.koronovskiy@gmail.com>
2020-12-19 17:21:34 +0400

    hw10 it3

--- homework10/parser.py
+++ homework10/parser.py
@@ -1,13 +1,19 @@
 """
-Получить информацию на сайте
+====информация из основной таблицы на сайте====
 
-- Текущая стоимость в рублях (конвертацию производить по текущему курсу, взятому с сайта центробанка РФ)
-- Код компании (справа от названия компании на странице компании)
-- P/E компании (информация находится справа от графика на странице компании)
-- Годовой рост/падение компании в процентах (основная таблица)
-- Высчитать какую прибыль принесли бы акции компании (в процентах), если бы они были куплены на уровне 52 Week Low и проданы на уровне 52 Week High (справа от графика на странице компании)
+- наименование компании +++
+- ссылка на страницу +++
+- годовая динамика в %
 
+====информация на странице компании====
 
+- последняя закрытая стоимость
+- последняя открытая стоимость
+
+- Код компании
+- P/E компании справа от графика
+
+- разница между '52 Week Low' и '52 Week High'
 """
 
 import requests
@@ -23,15 +29,13 @@ url = "https://markets.businessinsider.com/index/components/s&p_500"
 html_data = [row for row in parse_page(url).find_all("table")]
 
 
-keys = []  # all keys for parse
-for pos in html_data[1].find_all("th"):
-    keys.append(" ".join(pos.text.split()))
-keys = list(filter(None, keys))
-
-
-names = []  # company names
+names = []  # name, link, year_dynamics
 for actions in html_data[1].find_all("a"):
-    names.append(" ".join(actions.text.split()))
-print(names)
+    names.append(
+        {
+            "name": actions.text,
+            "link": "https://markets.businessinsider.com/" + actions.get("href"),
+        }
+    )
 
-# more code duplicates: prices, code, p/e, info per year - zip in dict, and transform
+print(names)
