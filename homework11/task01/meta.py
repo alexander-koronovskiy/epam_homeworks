1d1604768708d0a20aa555ecce39e8f6fd5227dd
alexander <alexander.koronovskiy@gmail.com>
2020-12-26 22:04:49 +0400

    hw11 t1 it6

--- homework11/task01/meta.py
+++ homework11/task01/meta.py
@@ -16,3 +16,25 @@ class Enum:
 
 Colors = Enum("RED", "BLUE", "ORANGE", "BLACK")
 Sizes = Enum("XL", "L", "M", "S", "XS")
+
+
+class Singleton(type):
+    def __init__(cls, name, bases, dic):
+        super(Singleton, cls).__init__(name, bases, dic)
+        cls.instance = None
+
+    def __call__(cls, *args, **kw):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+        return cls.instance
+
+
+class Meta:
+    __metaclass__ = Singleton
+
+    def __init__(self, a=None):
+        self.a = a
+
+
+c = Meta("parameter")
+print(c.a)
