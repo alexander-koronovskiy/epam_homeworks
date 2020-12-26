1dd6c523419c6bd9d2a7e6887832cfed62925769
alexander <alexander.koronovskiy@gmail.com>
2020-12-26 18:11:36 +0400

    hw11 t1 it4

--- homework11/task01/corrector.py
+++ homework11/task01/corrector.py
@@ -4,9 +4,22 @@ Remove duplications in variables declarations using metaclasses.
 """
 
 
+class MyMeta(type):
+    def __new__(cls, name, baseClasses, classdict):
+        return type.__new__(cls, name, baseClasses, classdict)
+
+
+class ColorsEnum(metaclass=MyMeta):
+    keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+
+class SizesEnum(metaclass=MyMeta):
+    keys = ("XL", "L", "M", "S", "XS")
+
+
 class Enum:
     def __init__(self, *args):
-        self.__keys = set(args)
+        self.__keys = args
 
     def __getattr__(self, key):
         if key in self.__keys:
@@ -16,5 +29,7 @@ class Enum:
 
 Colors = Enum("RED", "BLUE", "ORANGE", "BLACK")
 Sizes = Enum("XL", "L", "M", "S", "XS")
+
 assert Colors.RED == "RED"
 assert Sizes.XL == "XL"
+print(ColorsEnum.keys)
