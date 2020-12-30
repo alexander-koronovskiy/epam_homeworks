8b4448cc8bb5470899ba894ad3b89336e0a566aa
alexander <alexander.koronovskiy@gmail.com>
2020-12-30 19:15:52 +0400

    hw12 it2

--- homework12/task/myfiles.py
+++ homework12/task/myfiles.py
@@ -1,6 +1,13 @@
 import datetime
 
-from peewee import *
+from peewee import (
+    CharField,
+    DateTimeField,
+    Model,
+    PrimaryKeyField,
+    SqliteDatabase,
+    TextField,
+)
 
 db = SqliteDatabase("posts.db")
 
@@ -8,39 +15,49 @@ db = SqliteDatabase("posts.db")
 class Homework(Model):
     """
     Атрибуты:
+
     text - текст задания
     deadline - хранит объект datetime.timedelta с количеством дней на выполнение
     created - c точной датой и временем создания
     """
 
     id = PrimaryKeyField()
-    date = DateTimeField(default=datetime.datetime.now)
-    title = CharField()
     text = TextField()
+    created = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         database = db
 
 
 class Teacher(Model):
-    """"""
+    """
+    Атрибуты: last_name, first_name
+
+    Методы:
+    create_homework - текст задания и количество дней на это задание,
+    возвращает экземпляр Homework
+    """
 
     id = PrimaryKeyField()
-    date = DateTimeField(default=datetime.datetime.now)
-    title = CharField()
-    text = TextField()
+    name = CharField()
+    last_name = CharField()
 
     class Meta:
         database = db
 
 
 class Student(Model):
-    """"""
+    """
+    Атрибуты: last_name, first_name
+
+    Методы:
+    do_homework - принимает объект Homework и возвращает его же,
+    если задание уже просрочено, то печатет 'You are late' и возвращает None
+    """
 
     id = PrimaryKeyField()
-    date = DateTimeField(default=datetime.datetime.now)
-    first_name = CharField()
-    text = TextField()
+    name = CharField()
+    last_name = CharField()
 
     class Meta:
         database = db
@@ -54,10 +71,15 @@ def initialize_db():
 
 def add_row():
     initialize_db()
-    homework = Homework.create(id=8, title="Some title", text="some text1")
+    homework = Homework.create(id=1, title="Base python", text="")
     homework.save()
 
 
-db.connect()
-print(Homework.get(Homework.id == 1).date)
-db.close()
+def get_row():
+    db.connect()
+    print(Homework.get(Homework.id == 1).created)
+    db.close()
+
+
+if __name__ == "__main__":
+    get_row()
