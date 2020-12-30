66e8907e7a5455b0e35f35515dba261b98108410
alexander <alexander.koronovskiy@gmail.com>
2020-12-30 12:15:42 +0400

    hw12 it0

--- homework12/task/myfiles.py
+++ homework12/task/myfiles.py
@@ -2,12 +2,10 @@ import datetime
 
 from peewee import *
 
-# create database to interact with
 db = SqliteDatabase("posts.db")
 
 
-# create a class for blogposts
-class Post(Model):
+class Homework(Model):
     id = PrimaryKeyField()
     date = DateTimeField(default=datetime.datetime.now)
     title = CharField()
@@ -19,13 +17,10 @@ class Post(Model):
 
 def initialize_db():
     db.connect()
-    db.create_tables([Post], safe=True)
+    db.create_tables([Homework], safe=True)
     db.close()
 
 
-# if db tables are not created, create them
-# add a new row
-# persist it to db, not necessarily needed
 initialize_db()
-post = Post.create(id=1, title="Some title", text="some text1")
-post.save()
+homework = Homework.create(id=2, title="Some title", text="some text1")
+homework.save()
