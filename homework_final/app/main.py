1cd31fee6cfee8912dfd48e4ee1fe572a6cf5fcd
alexander <alexander.koronovskiy@gmail.com>
2021-01-05 18:06:29 +0400

    final it0

--- homework_final/app/main.py
+++ homework_final/app/main.py
@@ -8,4 +8,11 @@ user_list = RandomUser.generate_users(10)
 
 # вывести пол человека, имя, фамилию, номер телефона, email, место проживания
 for user in user_list:
-    print(user.get_phone())
+    print(
+        user.get_gender(),
+        user.get_first_name(),
+        user.get_last_name(),
+        user.get_phone(),
+        user.get_email(),
+        user.get_state(),
+    )
