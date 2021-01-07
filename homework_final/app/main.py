35820aa171893012229c795abf35a5886c2d32a6
alexander <alexander.koronovskiy@gmail.com>
2021-01-07 18:27:21 +0400

    final it04

--- homework_final/app/main.py
+++ homework_final/app/main.py
@@ -1,18 +1,62 @@
+import datetime
+
+from peewee import *
 from randomuser import RandomUser
 
 # Generate a single user
 user = RandomUser()
 
-# Generate a list of 10 random users
-user_list = RandomUser.generate_users(10)
+# create db
+db = SqliteDatabase("posts.db")
 
-# вывести пол человека, имя, фамилию, номер телефона, email, место проживания
-for user in user_list:
-    print(
-        user.get_gender(),
-        user.get_first_name(),
-        user.get_last_name(),
-        user.get_phone(),
-        user.get_email(),
-        user.get_state(),
+# Generate a list of 10 random users
+user_list = RandomUser.generate_users(1)
+
+
+class User(Model):
+
+    id = PrimaryKeyField()
+    first_name = TextField()
+    last_name = TextField()
+    gender = TextField()
+    phone = TextField()
+    email = TextField()
+    state = TextField()
+
+    created = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
+
+def initialize_db():
+    db.connect()
+    db.create_tables([User], safe=True)
+    db.close()
+
+
+def add_row():
+    user = user_list[0]
+
+    initialize_db()
+    user = User.create(
+        id=2,
+        first_name=user.get_first_name(),
+        last_name=user.get_last_name(),
+        gender=user.get_gender(),
+        phone=user.get_phone(),
+        email=user.get_email(),
+        state=user.get_state(),
     )
+
+    user.save()
+
+
+def get_row():
+    db.connect()
+    row = User.get(User.id == 2).state
+    db.close()
+    return row
+
+
+print(get_row())
