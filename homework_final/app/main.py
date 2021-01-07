611b1532fa1b7f08bd0fd43dcaf1a872aae26cba
alexander <alexander.koronovskiy@gmail.com>
2021-01-07 18:44:50 +0400

    final it05

--- homework_final/app/main.py
+++ homework_final/app/main.py
@@ -9,13 +9,10 @@ user = RandomUser()
 # create db
 db = SqliteDatabase("posts.db")
 
+
 # Generate a list of 10 random users
-user_list = RandomUser.generate_users(1)
-
-
 class User(Model):
 
-    id = PrimaryKeyField()
     first_name = TextField()
     last_name = TextField()
     gender = TextField()
@@ -36,27 +33,25 @@ def initialize_db():
 
 
 def add_row():
-    user = user_list[0]
+    user = RandomUser.generate_users(1)[0]
 
     initialize_db()
-    user = User.create(
-        id=2,
+    User.create(
         first_name=user.get_first_name(),
         last_name=user.get_last_name(),
         gender=user.get_gender(),
         phone=user.get_phone(),
         email=user.get_email(),
         state=user.get_state(),
-    )
-
-    user.save()
+    ).save()
 
 
 def get_row():
     db.connect()
-    row = User.get(User.id == 2).state
+    row = User.get(User.id == 7)
     db.close()
     return row
 
 
-print(get_row())
+# add_row()
+print(get_row().phone)
